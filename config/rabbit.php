bb71fc69f426c485495d053b49699ede114a3caa
Akbarali <39323182+akbarali1@users.noreply.github.com>
2025-01-26 17:26:42 +0500

    fix

--- config/rabbit.php
+++ config/rabbit.php
@@ -2,15 +2,15 @@
 
 return [
 	'configuration'     => [
-		'host'     => 'localhost',
-		'port'     => '5672',
-		'user'     => '',
-		'password' => '',
-		'vhost'    => '',
+		'host'     => env('RABBIT_HOST', '127.0.0.1'),
+		'port'     => env('RABBIT_PORT', '5672'),
+		'user'     => env('RABBIT_USER'),
+		'password' => env('RABBIT_PASSWORD'),
+		'vhost'    => env('RABBIT_VHOST'),
 	],
 	'available_locales' => ['uz', 'ru'],
 	
 	"middleware" => 'web',
 	"path"       => env('RABBIT_PATH', 'rabbitmq'),
-	"domain"     => env('RABBITMQ_DOMAIN'),
+	"domain"     => env('RABBIT_DOMAIN'),
 ];
