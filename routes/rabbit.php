bb2bb2c527d4b4ed627b2af494db7bf67da3c1ea
Akbarali <39323182+akbarali1@users.noreply.github.com>
2025-01-27 13:17:00 +0500

    fix

--- routes/rabbit.php
+++ routes/rabbit.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * @uses \Akbarali\RabbitListener\Console\RabbitChannelListener::handleMessage() at 151 and 167 lines
+ * @see  \Akbarali\RabbitListener\Dispatchers\RabbitMQChannelDispatcher::call();
+ */
 return [
 	//'one' => [
 	//	'service' => [RabbitService::class, 'functionOne'],
