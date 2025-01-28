b93040f67813cb707f63adbacd26674e6b0c4e71
Akbarali <39323182+akbarali1@users.noreply.github.com>
2025-01-28 15:23:43 +0500

    Update rabbit.php

--- routes/rabbit.php
+++ routes/rabbit.php
@@ -7,7 +7,8 @@ declare(strict_types=1);
  */
 return [
 	//'one' => [
-	//	'service' => [RabbitService::class, 'functionOne'],
-	//	'auth'    => true,
+	//	"service" => [RabbitService::class, 'functionOne'],
+	//	"auth"    => true,
+	//	"parameterType" => RegisterActionData::class,
 	//],
 ];
